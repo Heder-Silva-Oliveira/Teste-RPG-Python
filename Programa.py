83dd67d66fb0a827154420c766b2f69e6c7214ef
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-05-23 13:01:10 -0300

    Frist commit

---
+++ Programa.py
@@ -0,0 +1,3 @@
+criaturas = ('Orque', 'Elfo', 'Humano')
+classe = ('Guereiro', 'Caçador', 'Mago')
+atributos = {'Força': 0, 'Destreza': 0, 'Constituição': 0, 'Inteligência': 0,'Sabedoria': 0, 'Carisma': 0}
