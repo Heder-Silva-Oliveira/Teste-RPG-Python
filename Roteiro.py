eb89c80f05fb32f81b079d6f9c82a20396bb97f2
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-05-25 21:51:14 -0300

    Implementação 0004:

---
+++ Roteiro.py
@@ -0,0 +1,15 @@
+introdução = "O salão esta em silencio, todos estão focando suas energias para invocar," \
+             "mais uma aurea porem desta vez um nivel superior, você esta sentado no meio do pendagrama" \
+             "quando percebe uma pertibação nos seus sentidos, você ja não consegue distinguir o que é oufato ou tato" \
+             "oque é paladar ou audição, tudo esta confuso, você soua com friu, sente sede estando saciado" \
+             "você não sente o chão, você são sente o ar, você não ente nada."
+capitulo1 = ''
+escolha1 = 'Você tenta abrii os olhos, se esforça, você deixa de pensar nos textos lidos, nos rituais e palavras,' \
+           'e se consentra em se livrar desta senção, e por fim consegue seus olhos se abrem grandes e assustados, ' \
+           'todos seus sentidos voltam ao normal como se nada houvera mudado. E o sentimento vem forte em você o ' \
+           'sentimento de perda, você sabe que falhou, que jamais conseguirá novamente tentar conjurar essa aurea superior.'
+
+escolha2 = 'Você não se intimida com a força da aurea, você sabe que isso é apenas uma tentativa dela de não ser ' \
+           'conjurada, seu foco aumenta, você se consentra, pouco a pouco sua visão eteria começa a focar e distinguir' \
+           'seu objetivo, uma silueta é revelada, você encontrou nos confis do limbo, você esta de frente para a ' \
+           'portadora da aurea'
