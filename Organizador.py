b9f80c21803f783493f380be6ee490c7c910b9cf
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-06-28 22:43:00 -0300

    Implementação 0042:

--- Organizador.py
+++ Organizador.py
@@ -21,27 +21,28 @@ def menu(lista):
     print(linha(40))
 
 class Lutaesp:
-        def batalhadefesp(inimigoataq):
-            inimigoataq = randint(1, 20)
-            print(f'O ataque inimigo foi de {inimigoataq}, minha CA é de {12}')
-            if inimigoataq >= 12:
-                print('Você foi atingido')
-                dano = randint(1, 6)
-                if dano == 1:
-                    print('A espada passa em um arco descendente, porém apenas de raspam')
-                if dano == 2:
-                    print('A espada acerta seu alvo, um pequeno corte surge vermelho')
-                if dano == 3:
-                    print('O golpe foi certeiro, causando grande dor e deixando exposto no local acertado')
-                if dano == 4:
-                    print('A dor do ferimento é muito forte, deixando os olhos sem foco ')
-                if dano == 5:
-                    print('A dor é insuportável os joelho se dobram e a mão vai até no local atingido')
-                if dano == 6:
-                    print('O gole preciso o sangramento surge forte, causando o a queda ao chão e desfoque ')
-            else:
-                print('Você não foi atingido')
 
+
+    def batalhadefesp(inimigoataq):
+        inimigoataq = randint(1, 20)
+        print(f'O ataque inimigo foi de {inimigoataq}, minha CA é de {12}')
+        if inimigoataq >= 12:
+            print('Você foi atingido')
+            dano = randint(1, 6)
+            if dano == 1:
+                print('A espada passa em um arco descendente, porém apenas de raspam')
+            if dano == 2:
+                print('A espada acerta seu alvo, um pequeno corte surge vermelho')
+            if dano == 3:
+                print('O golpe foi certeiro, causando grande dor e deixando exposto no local acertado')
+            if dano == 4:
+                print('A dor do ferimento é muito forte, deixando os olhos sem foco ')
+            if dano == 5:
+                print('A dor é insuportável os joelho se dobram e a mão vai até no local atingido')
+            if dano == 6:
+                print('O gole preciso o sangramento surge forte, causando o a queda ao chão e desfoque ')
+        else:
+            print("Você não foi atingido")
 
 
     def batalhaatqesp(meuataq):
